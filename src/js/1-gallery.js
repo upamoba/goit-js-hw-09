acf4111487e1f1132aa8e70509722692bf1c0be2
uty <fgmapam@gmail.com>
2025-05-01 20:10:33 +0300

    summary(required)

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,5 +1,8 @@
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import SimpleLightbox from  "simplelightbox" ;
+
+import  "simplelightbox/dist/simple-lightbox.min.css" ;
+
 
 
 const images = [
